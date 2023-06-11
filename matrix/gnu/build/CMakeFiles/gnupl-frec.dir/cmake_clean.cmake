ebd6b7a4e9c99dcc3739c081b474a3b3330e2790
D5n9sMatrix <denis.02live@hotmail.com>
2023-06-10 21:41:24 -0300

    welcome gnupl-frec-plp18 math

---
+++ matrix/gnu/build/CMakeFiles/gnupl-frec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gnupl-frec.dir/gnupl-frec.cpp.o"
+  "CMakeFiles/gnupl-frec.dir/gnupl-frec.cpp.o.d"
+  "libgnupl-frec.a"
+  "libgnupl-frec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gnupl-frec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ matrix/gnu/build/CMakeFiles/gnupl-frec.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for gnupl-frec.
+# This may be replaced when dependencies are built.
